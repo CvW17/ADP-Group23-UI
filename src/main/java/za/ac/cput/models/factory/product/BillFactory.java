7f0d20b9451c7c3ff88ab08c98bdefd1399f0040
Cameron van Wyk <219088470@mycput.ac.za>
2021-10-01 10:46:41 +0200

    Added code to Bill factory

--- src/main/java/za/ac/cput/models/factory/product/BillFactory.java
+++ src/main/java/za/ac/cput/models/factory/product/BillFactory.java
@@ -4,6 +4,18 @@
 
 package za.ac.cput.models.factory.product;
 
+import za.ac.cput.models.entity.product.Bill;
+import za.ac.cput.util.GenericHelper;
+
 public class BillFactory
 {
+    public static Bill build(double subTotal, double tax, double total)
+    {
+        String billID = GenericHelper.generateID();
+
+        Bill bill = new Bill.Builder().setBillID(billID).setSubTotal(subTotal).setTax(tax)
+                .setTotal(total).build();
+
+        return bill;
+    }
 }
