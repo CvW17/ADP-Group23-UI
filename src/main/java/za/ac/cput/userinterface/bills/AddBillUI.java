aa91c9425b6d870bba3b301e425055e4d69f7341
Cameron van Wyk <219088470@mycput.ac.za>
2021-10-01 12:26:31 +0200

    Added code to AddBill interface

--- src/main/java/za/ac/cput/userinterface/bills/AddBillUI.java
+++ src/main/java/za/ac/cput/userinterface/bills/AddBillUI.java
@@ -5,19 +5,58 @@
 package za.ac.cput.userinterface.bills;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddBillUI extends JFrame implements ActionListener
 {
+    private JPanel pnlNorth, pnlCenter, pnlSouth;
+    private JButton btnSave;
+    private JLabel lblBillID, lblTax, lblSubTotal, lblTotal;
+    private JTextField txtBillID, txtTax, txtSubTotal, txtTotal;
+
     public AddBillUI()
     {
+        pnlNorth = new JPanel();
+        pnlCenter = new JPanel();
+        pnlSouth = new JPanel();
 
+        btnSave = new JButton("Save");
+
+        lblBillID = new JLabel("Bill ID: ");
+        lblSubTotal = new JLabel("Sub Total: ");
+        lblTax = new JLabel("Tax: ");
+        lblTotal = new JLabel("Total: ");
+
+        txtBillID = new JTextField();
+        txtSubTotal = new JTextField();
+        txtTax = new JTextField();
+        txtTotal = new JTextField();
     }
 
     public void setGUI()
     {
+        this.setTitle("Add new Bill");
 
+        pnlNorth.setLayout(new FlowLayout());
+
+        pnlCenter.setLayout(new GridLayout(4, 2));
+        pnlCenter.add(lblBillID);
+        pnlCenter.add(txtBillID);
+        pnlCenter.add(lblSubTotal);
+        pnlCenter.add(txtSubTotal);
+        pnlCenter.add(lblTax);
+        pnlCenter.add(txtTax);
+        pnlCenter.add(lblTotal);
+        pnlCenter.add(txtTotal);
+
+        pnlSouth.add(btnSave);
+        btnSave.addActionListener(this);
+
+        this.add(pnlNorth, BorderLayout.NORTH);
+        this.add(pnlCenter, BorderLayout.CENTER);
+        this.add(pnlSouth, BorderLayout.SOUTH);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(400, 400);
